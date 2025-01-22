9f2c9f5849d3f3f65107bd1e85d9f4465059d294
Himanshu choudhary <86661174+Iamhc@users.noreply.github.com>
2025-01-22 17:16:49 +0530

    Update contactform.jsx

--- my-app/src/components/contactform.jsx
+++ my-app/src/components/contactform.jsx
@@ -57,7 +57,7 @@ const ContactForm = () => {
           <img
             src={require("../resources/t.png")}
             alt="Location Map"
-            className="w-full h-[300px] lg:h-full object-contain"
+            className="w-full h-[110%] lg:h-full object-contain"
           />
         </a>
       </div>
